71423938a66f162547b371c80c5ee64aeb900a53
找调网 <market@zhaodiao.net>
2020-05-06 17:46:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -42,7 +42,7 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
+        'agama.worker.dev': 1,
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
